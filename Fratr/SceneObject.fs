845dfadc1969391ecf10c87d3655c6e34e8cceac
Ben Gladman <ben.gladman@gmail.com>
2016-05-02 15:21:42 +0100

    More efficient sphere

--- Fratr/SceneObject.fs
+++ Fratr/SceneObject.fs
@@ -19,36 +19,37 @@ module SceneObject =
         HitTest: Ray -> HitResult option
         }
 
-    let private solveQuadEquation (a : float,  b : float, c : float) =
-        let rad = b*b - 4.0*a*c
-        let k0 = b / (-2.0*a)
-        match rad with
-        | Negative -> []
-        | NearZero -> [ k0 ]
-        | Positive -> let sqrtRad = (sqrt rad)/(2.0 * a)
-                      [ k0 + sqrtRad; k0 - sqrtRad ]
-
     let Sphere (center: Point) (radius: float) (material: Material) : SceneObject =
         let r2 = radius * radius
-        let hitTest (ray : Ray) =
-            
-            let getResult t =
-                let pos = ray.Start + t * ray.Direction
+
+        let hitTest2 (ray: Ray) =
+            let v0 = center - ray.Start
+            let k0 = (Dot (v0, ray.Direction))
+            let b = -2.0 * k0
+            let c = (Dot (v0, v0)) - r2
+            let rad = b * b - 4.0 * c
+
+            let dist =
+                match rad with
+                | Negative -> 0.0
+                | NearZero -> k0
+                | Positive ->
+                    let sqrtRad = (sqrt rad) / 2.0
+                    let root1 = k0 - sqrtRad
+                    if root1 |> IsPositive then
+                        root1
+                    else
+                        k0 + sqrtRad
+
+            if dist |> IsPositive then
+                let pos = ray.Start + dist * ray.Direction
                 let normal = Norm (pos - center)
-                { Ray = ray; Distance = t; Pos = pos; Normal = normal; Material = material }
-
-            let ts =
-                let v0 = center - ray.Start
-                let b = -2.0*(Dot (v0,ray.Direction))
-                let c = (Dot (v0,v0)) - r2
-                solveQuadEquation (1.0, b, c)
-
-            match ts |> List.filter IsPositive with
-            | [t]    -> t |> getResult |> Some
-            | [a; b] -> (min a b) |> getResult |> Some
-            | _      -> None
-
-        { HitTest = hitTest }
+                { Ray = ray; Distance = dist; Pos = pos; Normal = normal; Material = material }
+                |> Some
+            else
+                None
+                                
+        { HitTest = hitTest2 }
 
     let Plane (pointOnPlane: Point) (normal: Direction) (material: Material) : SceneObject =
         let hitTest (ray: Ray) =
